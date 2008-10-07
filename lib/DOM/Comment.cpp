ebcd8db4277b8b02d1514b75559e9645d55020a5
cHoBi <chobi.noob@gmail.com>
2008-10-07 19:24:03 +0200

    :3

---
+++ lib/DOM/Comment.cpp
@@ -0,0 +1,55 @@
+/****************************************************************************
+* XML++ is a library for working with XML files.                            *
+* Copyleft meh.                                                             *
+*                                                                           *
+* This file is part of XML++                                                *
+*                                                                           *
+* XML++ is free software: you can redistribute it and/or modify             *
+* it under the terms of the GNU Affero General Public License as            *
+* published by the Free Software Foundation, either version 3 of the        *
+* License, or (at your option) any later version.                           *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU Affero General Public License for more details.                       *
+*                                                                           *
+* You should have received a copy of the GNU Affero General Public License  *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+****************************************************************************/
+
+#include "Comment.h"
+
+Comment::Comment (Document* ownerDocument) : CharacterData (ownerDocument, Node::COMMENT_NODE)
+{
+
+}
+
+// Parent realization.
+DOMString
+Comment::nodeName (void)
+{
+    return (DOMString) "#comment";
+}
+
+DOMString
+Comment::nodeValue (void)
+{
+    return this->data();
+}
+
+void
+Comment::nodeValue (const DOMString& value) throw()
+{
+    this->data(value);
+}
+
+Node*
+Comment::cloneNode (bool deep)
+{
+    Comment* comment = new Comment(this->ownerDocument());
+    comment->nodeValue(this->data());
+
+    return comment;
+}
+
---
+++ lib/DOM/Comment.h
@@ -0,0 +1,55 @@
+/****************************************************************************
+* XML++ is a library for working with XML files.                            *
+* Copyleft meh.                                                             *
+*                                                                           *
+* This file is part of XML++                                                *
+*                                                                           *
+* XML++ is free software: you can redistribute it and/or modify             *
+* it under the terms of the GNU Affero General Public License as            *
+* published by the Free Software Foundation, either version 3 of the        *
+* License, or (at your option) any later version.                           *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU Affero General Public License for more details.                       *
+*                                                                           *
+* You should have received a copy of the GNU Affero General Public License  *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+****************************************************************************/
+
+#if !defined(XMLPP_DOM_COMMENT_H)
+#define XMLPP_DOM_COMMENT_H
+
+#include "../common.h"
+#include "CharacterData.h"
+
+namespace xmlpp {
+
+namespace DOM {
+
+class Comment : public CharacterData
+{
+  protected:
+    Comment (Document* ownerDocument);
+
+  public:
+    friend class Document;
+
+  // Parent realization.
+  public:
+    DOMString nodeName (void);
+
+    DOMString nodeValue (void) throw();
+    void nodeValue (const DOMString& value) throw();
+
+    Node* cloneNode (bool deep = true);
+
+};
+
+};
+
+};
+
+#endif
+
